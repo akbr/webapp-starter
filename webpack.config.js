2582f28d12a28344f0548ee52142e7913537460c
Aaron <akbr@akbr.net>
2016-08-25 15:35:14 -0400

    renamed app to src

--- webpack.config.js
+++ webpack.config.js
@@ -4,21 +4,24 @@ var pkg = require("./package.json")
 
 module.exports = {
   entry: {
-    app: ['./src/app.js'],
+    index: ['./src/index.js'],
     vendor: Object.keys(pkg.dependencies),
   },
   output: {
     path: path.resolve("bin"),
     publicPath: "/bin/",
-    filename: 'app.bundle.js'
+    filename: 'index.bundle.js'
   },
-  devtool: "cheap-module-eval-source-map",
+  devtool: "cheap-module-source-map",
   module: {
     loaders: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
     }]
+  }
+  node: {
+    fs: "empty"
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin("vendor", "vendor.bundle.js")
